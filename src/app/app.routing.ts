d4b52a6b22a4d58481c1c236e26584c6302cb1ba
Mark <mark.aaron.fisher@gmail.com>
2017-05-12 14:01:32 -0700

    add about route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,17 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MembersDisplayComponent} from './members-display/members-display.component';
+import {AboutComponent} from './about/about.component'
 
 
 const appRoutes: Routes = [
   {
   path: 'members',
   component: MembersDisplayComponent
+},
+{
+path: 'about',
+component: AboutComponent
 }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
